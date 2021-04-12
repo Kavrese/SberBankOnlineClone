cf077c68bc98a6959ef01c4ea69f8817ff66e892
Kavrese <57292303+Kavrese@users.noreply.github.com>
2021-04-12 20:01:48 +0300

    2 Update MainActivity XML

--- app/src/main/java/com/example/sberclone/MainActivity.kt
+++ app/src/main/java/com/example/sberclone/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.sberclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,7 +13,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         window.setFlags(
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
         )
+
+
+    }
+
+    fun clickSelects(v: View){
+        for (i in arrayListOf<TextView>(select1, select2, select3)) {
+            i.setTextColor(getColor(R.color.colorHint2))
+        }
+        (v as TextView).setTextColor(getColor(R.color.colorText1))
     }
 }
