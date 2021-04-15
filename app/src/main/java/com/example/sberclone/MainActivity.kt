ddcc5878aafb56d1bd9b5020854d8a7c36744cd9
Kavrese <57292303+Kavrese@users.noreply.github.com>
2021-04-15 22:26:41 +0300

    Add Scroll Categories (already only cards)

--- app/src/main/java/com/example/sberclone/MainActivity.kt
+++ app/src/main/java/com/example/sberclone/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.sberclone
 
+import android.animation.LayoutTransition
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,7 +18,18 @@ class MainActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
         )
 
+        val clickToLin = View.OnClickListener{
+            if (it.layoutParams.height != ViewGroup.LayoutParams.WRAP_CONTENT){
+                showFull(it)
+            }else{
+                hideFull(it)
+            }
+        }
 
+        for (i in arrayListOf(cards_lin, investor_lin, strah_lin, val_lin, vklads_lin, target_lin, security_lin, credits_lin)) {
+            i.setOnClickListener(clickToLin)
+            (i as ViewGroup).layoutTransition.enableTransitionType(LayoutTransition.CHANGING)
+        }
     }
 
     fun clickSelects(v: View){
@@ -24,5 +37,17 @@ class MainActivity : AppCompatActivity() {
             i.setTextColor(getColor(R.color.colorHint2))
         }
         (v as TextView).setTextColor(getColor(R.color.colorText1))
+    }
+
+    private fun showFull(view: View){
+        val params =  view.layoutParams
+        params.height = ViewGroup.LayoutParams.WRAP_CONTENT
+        view.layoutParams = params
+    }
+
+    private fun hideFull(view: View){
+        val params =  view.layoutParams
+        params.height = resources.getDimension(R.dimen.numdp).toInt()
+        view.layoutParams = params
     }
 }
